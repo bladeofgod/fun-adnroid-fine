bee35bd6eddebb1b6700c2fe5c858a9aaaf0c029
lijiaqi <aichonghui>
2019-09-03 15:20:34 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/yd188/flutter_app_funandroid/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.yd188.flutter_app_funandroid
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
